a2b0d7b6481efc15305a29a99bd18625b06a8a52
KateDetsyk <detsyk.k@ucu.edu.ua>
2021-12-21 16:56:03 +0200

    add constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,38 @@
 // Constants will be here
+
+import 'package:flutter/material.dart';
+
+const sandColor = const Color(0xFFF8D090);
+const blackColor = const Color(0xFF0A0A0A);
+const orangeColor = const Color(0xFFD08718);
+
+
+class AppTextFonts {
+  static const TextStyle Raleway20BlackSemiBold = TextStyle(
+      color: blackColor,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Raleway",
+      fontSize: 20);
+
+  static const TextStyle Raleway24BlackBold = TextStyle(
+      color: blackColor,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Raleway",
+      fontSize: 24);
+
+  static const TextStyle Raleway20BlackRegular = TextStyle(
+      color: blackColor,
+      // fontWeight: FontWeight.w500,
+      fontFamily: "Raleway",
+      fontSize: 20);
+
+  static const TextStyle Raleway30WhiteBold = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Raleway",
+      fontSize: 30);
+}
+
+// const defaultUserLogo = "assets/images/icons/witch.png";
+// const humanImg = "assets/images/icons/human.png";
+// const img_01 = "assets/images/img_01.png";
